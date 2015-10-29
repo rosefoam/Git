be968a6c6f93d5664819a8790eb0d2119f82aa75
rosefoam <rosefoam2010@163.com>
2015-10-28 22:11:54 -0500

    新建动态JAVA工程

---
+++ src/com/atguigu/git/Person.java
@@ -0,0 +1,23 @@
+package com.atguigu.git;
+
+public class Person {
+	
+	private String name;
+	private Integer age;
+	public void info(){
+		System.out.println("不要问我从哪里来");
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+}
